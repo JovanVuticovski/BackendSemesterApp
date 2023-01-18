5ce496594f0c690b8a95dc56f7fe0aa7edc577a1
jovan_vuticovski <jovan.vuticovski@utb.ecutbildning.se>
2023-01-18 11:56:08 +0100

    Changed repository classes to interfaces

--- src/main/java/com/example/backendsemesterapp/repository/UserRepository.java
+++ src/main/java/com/example/backendsemesterapp/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.example.backendsemesterapp.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository {
 }
