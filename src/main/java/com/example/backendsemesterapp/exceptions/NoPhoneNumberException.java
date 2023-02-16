4985261cbc86cea85769cec4605fe9774518b486
jovan_vuticovski <jovan.vuticovski@utb.ecutbildning.se>
2023-02-16 16:40:25 +0100

    Added exception UserExistsAlready

--- src/main/java/com/example/backendsemesterapp/exceptions/NoPhoneNumberException.java
+++
@@ -1,4 +0,0 @@
-package com.example.backendsemesterapp.exceptions;
-
-public class NoPhoneNumberException extends Exception {
-}
--- src/main/java/com/example/backendsemesterapp/exceptions/UserExistAlreadyException.java
+++ src/main/java/com/example/backendsemesterapp/exceptions/UserExistAlreadyException.java
@@ -1,4 +1,9 @@
 package com.example.backendsemesterapp.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "This User already exists")
 public class UserExistAlreadyException extends Exception{
 }
